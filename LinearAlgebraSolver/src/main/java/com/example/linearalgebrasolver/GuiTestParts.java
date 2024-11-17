0f0e99455f3f58f91fe228cdd431fdf04965d93b
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-11-17 08:49:16 -0500

    Testing our main gui

---
+++ LinearAlgebraSolver/src/main/java/com/example/linearalgebrasolver/GuiTestParts.java
@@ -0,0 +1,16 @@
+package com.example.linearalgebrasolver;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GuiTestParts extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- LinearAlgebraSolver/src/main/java/com/example/linearalgebrasolver/MainPageUI.java
+++ LinearAlgebraSolver/src/main/java/com/example/linearalgebrasolver/MainPageUI.java
@@ -1,4 +1,43 @@
 package com.example.linearalgebrasolver;
 
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
 public class MainPageUI {
+
+    public static Scene mainScene(){
+
+        TabPane tabPane = new TabPane();
+
+        tabPane.getTabs().add(new Tab("Matrix Operations"));
+        tabPane.getTabs().add(new Tab("Planes and lines D"));
+
+
+
+
+
+        Scene scene = new Scene(tabPane);
+        return scene;
+    }
+    public void pointsOptions(TabPane tabPane){
+        Tab distanceTab = new Tab("Distance Calculations");
+        distanceTab.setClosable(false);
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(10));
+
+        Label selectLabel = new Label("Select Distance Type:");
+        ComboBox<String> distanceTypeCombo = new ComboBox<>();
+        distanceTypeCombo.getItems().addAll(
+                "Plane to Point Distance",
+                "Plane to Line Distance",
+                "Line to Point Distance"
+        );
+        distanceTypeCombo.setValue("Plane to Point Distance");
+        layout.getChildren().addAll(selectLabel, distanceTypeCombo);
+        distanceTab.setContent(layout);
+        tabPane.getTabs().add(distanceTab);
+    }
 }
